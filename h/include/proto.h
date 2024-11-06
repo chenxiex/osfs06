f2cb06610e20673ad256645d20f7e120dc41bcef
Anlor <chenxiex@outlook.com>
2024-11-06 20:48:52 +0800

    patch /h/

--- h/include/proto.h
+++ h/include/proto.h
@@ -5,6 +5,9 @@
                                                     Forrest Yu, 2005
 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
+/* i8259.c */
+PUBLIC void init_8259A();
+
 /* klib.asm */
 PUBLIC void	out_byte(u16 port, u8 value);
 PUBLIC u8	in_byte(u16 port);
@@ -17,6 +20,7 @@ PUBLIC u32	seg2phys(u16 seg);
 
 /* klib.c */
 PUBLIC void	delay(int time);
+PUBLIC void disp_int(int input);
 
 /* kernel.asm */
 void restart();
